3b52809bf3bd965c1620ff7126da4397d9045414
Douglass Coronado <dcoronado@funio.com>
2023-10-20 20:56:25 -0400

    Read yaml

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
-locals {
-  cluster_names = ["MontrealCollegeCluster1","MontrealCollegeCluster2","MontrealCollegeCluster3","MontrealCollegeCluster4"]
+#locals {
+#  cluster_names = ["MontrealCollegeCluster1","MontrealCollegeCluster2","MontrealCollegeCluster3","MontrealCollegeCluster4"]
+#
+#}
 
-}
+
+locals{
+    kubernetes_file_list = [for f in fileset("${path.module}/files", "[^_]*.yaml") : yamldecode(file("${path.module}/files/${f}"))]
+
+
+    cluster_names = flatten([
+      for cluster_list in local.kubernetes_file_list:[
+        for cluster in try(cluster_list.kubernetes_list,[]):{
+          name=cluster.name
+        }
+      ]
+    ])
+
 
 
 resource "azurerm_resource_group" "mcit" {
