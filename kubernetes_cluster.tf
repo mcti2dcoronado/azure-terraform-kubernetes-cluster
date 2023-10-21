a30f9b3b247ef2915954f3dc56f486ba6379b76f
Douglass Coronado <dcoronado@funio.com>
2023-10-20 21:41:26 -0400

    Add value

--- kubernetes_cluster.tf
+++ kubernetes_cluster.tf
@@ -1,6 +1,6 @@
 resource "azurerm_kubernetes_cluster" "MontrealCollege" {
-  for_each            = { for index,cluster in local.cluster_names : cluster => cluster }
-  name                = "${var.convention}-${each.key}"
+  for_each            = { for index,cluster in local.cluster_names : index => cluster }
+  name                = "${var.convention}-${each.value.name}"
   location            = azurerm_resource_group.mcit.location
   resource_group_name = azurerm_resource_group.mcit.name
   dns_prefix          = "${var.prefix}-k8s"
